617f4a3a294c2f55ba34c85601295c85d4ee5c46
bruno-reis <13205396+bruno-reis@users.noreply.github.com>
2018-03-10 08:31:17 -0300

    added example login route

--- module1/authBcrypt.js
+++ module1/authBcrypt.js
@@ -1,18 +1,47 @@
 const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
 
-const password = 'secret'
 let dbPassword;
 const saltRounds = 8
 
-bcrypt
-  .hash(password, saltRounds)
-  .then( hashedPassword => {
-    console.log('hash', hashedPassword)
-    return hashedPassword
+
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.pre('save', next => {
+  if (!this.isModified('password')) return next()
+
+  return bcrypt
+    .hash(this.password, saltRounds)
+    .then( hashedPassword => {
+      this.password = hashedPassword
+      return next()
+    })
+    .catch(err => {
+      return next(err)
+    })
+})
+
+userSchema.methods.comparePassword = (candidatePassword, next) => {
+  return bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return next(err)
+
+    return next(null, isMatch)
   })
-  .then( hash => {
-    return bcrypt.compare(password, hash)
-  })
-  .then( res => {
-    console.log('match', res)
-  })
+}
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
+
+
+
---
+++ module1/routes.js
@@ -0,0 +1,16 @@
+'use strict'
+router.post('/login', (req, res, next) => {
+  return User.findOne({ username: req.body.username})
+    .then(user => {
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (isMatch) {
+          return res.send('logged in!')
+        } else {
+          return res.redirect('/users/login')
+        }
+      })
+    })
+    .catch(err => {
+      return res.send(err)
+    })
+})
